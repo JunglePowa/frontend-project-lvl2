a5c6c8268f212f93dfbf1162e003fcf7ae7e1508
Yaroslav Sukhomlinov <yar.mail.box@gmail.comgitclear>
2022-05-15 21:47:05 +0300

    step 3

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/JunglePowa/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/JunglePowa/frontend-project-lvl2/actions)
+
+https://asciinema.org/a/nRrT1lVlhdrIYHNhxX8eoC8dq
+
+#### Сравнение плоского JSON
+
+для запуска игры наберите brain-calc
+
+[![JSON-diff](https://asciinema.org/a/nRrT1lVlhdrIYHNhxX8eoC8dq.svg)](https://asciinema.org/a/nRrT1lVlhdrIYHNhxX8eoC8dq)
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
 import { Command } from 'commander/esm.mjs';
+import { genDiff } from '../src/index.js';
 
 const program = new Command();
 program
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format')
-  .action((filepath1, filepath2, options) => {
-    console.log(filepath1, filepath2, options.format);
+  .argument('<filepath1>', 'path to file 1')
+  .argument('<filepath2>', 'path to file 2')
+  .action((filepath1, filepath2) => {
+    genDiff(filepath1, filepath2)
   });
 
-program.parse(process.argv);
-
+program.parse();
